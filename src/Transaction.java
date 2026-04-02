e54bbada9a388ead578d9da101645289a6d3bf95
rusperres <jairusjaspervc@gmail.com>
2026-04-02 19:25:02 +0800

    Added Transaction class

---
+++ src/Transaction.java
@@ -0,0 +1,25 @@
+public class Transaction {
+    private Request request;
+    private Response response;
+
+    public Transaction(Request request, Response response) {
+        this.request = request;
+        this.response = response;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public Response getResponse() {
+        return response;
+    }
+
+    public void setResponse(Response response) {
+        this.response = response;
+    }
+}
